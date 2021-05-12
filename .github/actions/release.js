526a8cc81f82cb7c5f13cc3853fddda119e315aa
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-12 17:19:49 -0400

    id

--- .github/actions/release.js
+++ .github/actions/release.js
@@ -26,9 +26,9 @@ async function run() {
     throw new Error(`Previous release not found! release: ${JSON.stringify(release)}`)
   }
 
-  const { release_id, name, prerelease } = release
+  const { id, name, prerelease } = release
 
-  if (release_id == null) {
+  if (id == null) {
     throw new Error(`Previous release id not found! release: ${JSON.stringify(release)}`)
   }
 
@@ -36,12 +36,12 @@ async function run() {
     throw new Error(`Previous release was not a pre-release. Only pre-releases can be upgraded to a release. release: ${JSON.stringify(release)}`)
   }
 
-  console.log(`Updating release ${name} (id: ${release_id}) to latest`)
+  console.log(`Updating release ${name} (id: ${id}) to latest`)
 
   await octokit.request('PATCH /repos/{owner}/{repo}/releases/{release_id}', {
     owner: 'meljameson',
     repo: 'test-gha',
-    release_id,
+    release_id: id,
     prerelease: false,
     tag_name: 'release'
   })
