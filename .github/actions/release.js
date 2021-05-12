06b1cb71a1e4b2abb5d604caeeed4a3a94c1733f
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-12 17:11:52 -0400

    data

--- .github/actions/release.js
+++ .github/actions/release.js
@@ -3,38 +3,33 @@ const { Octokit } = require("@octokit/action")
 const octokit = new Octokit()
 
 async function run() {
-  const releases = await octokit.request('GET /repos/{owner}/{repo}/releases', {
+  const response = await octokit.request('GET /repos/{owner}/{repo}/releases', {
     owner: 'meljameson',
     repo: 'test-gha',
   })
-
-  console.log({releases})
-
-  const tags = await octokit.request('GET /repos/{owner}/{repo}/tags', {
-    owner: 'octocat',
-    repo: 'hello-world'
-  })
-
-  console.log({tags})
   
-  if (releases == null) {
-    throw new Error('No releases found!')
+  if (response == null) {
+    throw new Error(`Unable to get release data from GitHub. No response: ${response}`)
   }
 
-  const release = releases[0]
+  const releases = response.data
 
+  if (releases == null) {
+    throw new Error(`No releases found! releases: ${releases}`)
+  }
+  
   if (release == null) {
-    throw new Error('Previous release not found!')
+    throw new Error(`Previous release not found! release: ${release}`)
   }
 
   const { release_id, name, prerelease } = release
 
   if (release_id == null) {
-    throw new Error('Previous release id not found!')
+    throw new Error(`Previous release id not found! release: ${release}`)
   }
 
   if (prerelease === false) {
-    throw new Error('Previous release was not a pre-release. Only pre-releases can be upgraded to a release.')
+    throw new Error(`Previous release was not a pre-release. Only pre-releases can be upgraded to a release. release: ${release}`)
   }
 
   console.log(`Updating release ${name} (id: ${release_id}) to latest`)
