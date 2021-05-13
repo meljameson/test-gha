5d4c50395d3637c84046d3349a734d67befa7b0e
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-13 16:18:19 -0400

    catch git error

--- release/checkout-prerelease.js
+++ release/checkout-prerelease.js
@@ -4,84 +4,89 @@
  * stash
  * git checkout release
  */
-const { exec } = require('child_process')
-const { Octokit } = require('octokit')
-
- async function getAllReleases() {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  // get list of releases
-  const response = await octokit.request('GET /repos/{owner}/{repo}/releases', {
-    owner: 'meljameson',
-    repo: 'test-gha',
-  })
-  
-  if (response == null) {
-    throw new Error(`Unable to get release data from GitHub. No response: ${JSON.stringify(response)}`)
-  }
-
-  return response.data
-}
-
-async function getLastPreRelease() {
-  // get the latest pre-release
-  const releases = await getAllReleases()
-
-  if (releases == null) {
-    throw new Error(`No releases found! releases: ${JSON.stringify(releases)}`)
-  }
-
-  // the last release is the first in the release list
-  const release = releases[0]
-  
-  if (release == null) {
-    throw new Error(`Previous release not found! release: ${JSON.stringify(release)}`)
-  }
-
-  return release
-}
-
-function execGitCmd(cmd) {
-    return new Promise((resolve, reject) => {
-        console.log('\x1b[36m%s\x1b[0m', `'${cmd}' is executing...`);
-        exec(cmd, function (error, stdout, stderr) {
-            if (error) reject(error)
-            if (stderr) reject(stderr)
-            resolve(stdout)
-        });
-    })
-}
-
-function checkForChanges() {
-  return execGitCmd('git diff --name-only && git diff --name-only --staged | sort | uniq')
-}
-
-(async function checkoutPrerelease() {
-  const changes = await checkForChanges()
-  if (changes) {
-    console.log(`Unsaved changes found: ${changes}`)
-    console.log('Stashing changes')
-    await execGitCmd('git stash -u')
-  }
-
-  const moreChanges = await checkForChanges()
-  if (moreChanges) {
-    console.log(`More unsaved changes found: ${moreChanges}. Exiting for human help`)
-    process.exit(1)
-  }
-  
-  await execGitCmd('git fetch')
-  const latest = await getLastPreRelease();
-  if (latest == null) {
-    console.log('No latest prerelease to check out')
-    process.exit(1)
-  }
-  if (latest.prerelease === false) {
-    console.log('No pre-release found needing to be smoketested')
-    process.exit(1)
-  }
-
-  console.log(`Checking out latest prerelease: ${latest.tag_name} at ${latest.url}`)
-  await execGitCmd(`git checkout ${latest.tag_name}`)
-  await execGitCmd(`git status`)
-  console.log('Done')
-})()
+ const { exec } = require('child_process')
+ const { Octokit } = require('octokit')
+ 
+  async function getAllReleases() {
+   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+   // get list of releases
+   const response = await octokit.request('GET /repos/{owner}/{repo}/releases', {
+     owner: 'meljameson',
+     repo: 'test-gha',
+   })
+   
+   if (response == null) {
+     throw new Error(`Unable to get release data from GitHub. No response: ${JSON.stringify(response)}`)
+   }
+ 
+   return response.data
+ }
+ 
+ async function getLastPreRelease() {
+   // get the latest pre-release
+   const releases = await getAllReleases()
+ 
+   if (releases == null) {
+     throw new Error(`No releases found! releases: ${JSON.stringify(releases)}`)
+   }
+ 
+   // the last release is the first in the release list
+   const release = releases[0]
+   
+   if (release == null) {
+     throw new Error(`Previous release not found! release: ${JSON.stringify(release)}`)
+   }
+ 
+   return release
+ }
+ 
+ function execGitCmd(cmd) {
+     return new Promise((resolve, reject) => {
+         console.log('\x1b[36m%s\x1b[0m', `'${cmd}' is executing...`);
+         exec(cmd, function (error, stdout, stderr) {
+           console.log({error, stdout, stderr})
+             if (error) reject(error)
+             if (stderr) reject(stderr)
+             resolve(stdout)
+         });
+     })
+ }
+ 
+ function checkForChanges() {
+   return execGitCmd('git diff --name-only && git diff --name-only --staged | sort | uniq')
+ }
+ 
+ (async function checkoutPrerelease() {
+   const changes = await checkForChanges()
+   if (changes) {
+     console.log(`Unsaved changes found: ${changes}`)
+     console.log('Stashing changes')
+     await execGitCmd('git stash -u')
+   }
+ 
+   const moreChanges = await checkForChanges()
+   if (moreChanges) {
+     console.log(`More unsaved changes found: ${moreChanges}. Exiting for human help`)
+     process.exit(1)
+   }
+ 
+   const latest = await getLastPreRelease();
+   if (latest == null) {
+     console.log('No latest prerelease to check out')
+     process.exit(1)
+   }
+   if (latest.prerelease === false) {
+     console.log('No pre-release found needing to be smoketested')
+     process.exit(1)
+   }
+ 
+   await execGitCmd('git fetch')
+   console.log(`Checking out latest prerelease: ${latest.tag_name} at ${latest.url}`)
+   try {
+     await execGitCmd(`git checkout ${latest.tag_name}`)
+   } catch(e){
+     console.log(e)
+   }
+   await execGitCmd(`git status`)
+   console.log('Done')
+ })()
