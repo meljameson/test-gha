83c2590e1671526f242c95f5bbaaf217d17b61ec
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-13 10:17:53 -0400

    time

--- .github/actions/changelog.js
+++
@@ -1,5 +0,0 @@
-const { Octokit } = require("@octokit/action");
-
-const octokit = new Octokit();
-
-console.log('hello world!')
--- .github/actions/prerelease.js
+++ .github/actions/prerelease.js
@@ -1,9 +1,15 @@
+/**
+ * Create a release with prerelease: true in GitHub UI
+ * 
+ */
 const { Octokit } = require("@octokit/action");
 
 const octokit = new Octokit();
 
 async function run() {
-  const name = `${new Date()}-rc`;
+  const date = new Date();
+  const name = `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}-${date.getTime()}-alpha`
+
   await octokit.request('POST /repos/{owner}/{repo}/releases', {
     owner: 'meljameson',
     repo: 'test-gha',
@@ -18,4 +24,8 @@ async function run() {
   octokit.log.error(`Hellooooo`)
 }
 
-run()
+try {
+  run()
+} catch(e) {
+  octokit.log.error(e)
+}
