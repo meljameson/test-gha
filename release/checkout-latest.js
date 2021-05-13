a822147c19f85039d6b0129fc1025d679d6e2d8b
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-13 15:32:09 -0400

    stash

--- release/checkout-latest.js
+++ release/checkout-latest.js
@@ -1,8 +1,33 @@
-const { Octokit } = require("@octokit/action");
+/**
+ * take stock of changes
+ * tell slack of changed files
+ * stash
+ * git checkout release
+ */
+const { exec } = require('child_process');
 
-const octokit = new Octokit();
+function execGitCmd(cmd) {
+    return new Promise((resolve, reject) => {
+        console.log('\x1b[36m%s\x1b[0m', `"${cmd}" is executing...`);
+        exec(cmd, function (error, stdout, stderr) {
+            if (error) reject(error)
+            if (stderr) reject(stderr)
+            resolve(stdout)
+        });
+    })
+}
 
-await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
-  owner: 'meljameson',
-  repo: 'test-gha'
-})
+async function run() {
+  const prestash = await execGitCmd('git diff --name-only && git diff --name-only --staged | sort | uniq')
+  console.log({prestash})
+  await execGitCmd('git stash')
+  const poststash = await execGitCmd('git diff --name-only && git diff --name-only --staged | sort | uniq')
+  console.log({poststash})
+
+  await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+    owner: 'meljameson',
+    repo: 'test-gha'
+  })
+}
+
+run()
