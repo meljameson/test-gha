4c5b55c6de273fab1925a2418039d80f5f8c8fb7
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-13 09:49:01 -0400

    date-rc tag

--- .github/actions/prerelease.js
+++ .github/actions/prerelease.js
@@ -3,16 +3,19 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 
 async function run() {
+  const name = `${new Date()}-rc`;
   await octokit.request('POST /repos/{owner}/{repo}/releases', {
     owner: 'meljameson',
     repo: 'test-gha',
-    tag_name: 'v0-prelease',
+    tag_name: name,
+    name,
     prerelease: true,
-    body: `
-    * helloooooo
-    * testing
-    `,
+    body: "\
+    * helloooooo\
+    * testing\
+    ",
   })
+  octokit.log.error(`Hellooooo`)
 }
 
 run()
--- .github/actions/release.js
+++ .github/actions/release.js
@@ -1,9 +1,13 @@
-const { Octokit } = require("@octokit/action")
+const { Octokit } = require('@octokit/action')
 
 const octokit = new Octokit()
 
-async function run() {
-  // get a list of releases
+/**
+ * https://docs.github.com/en/rest/reference/repos#list-releases
+ * @returns Array<Object>
+ */
+async function getAllReleases() {
+  // get list of releases
   const response = await octokit.request('GET /repos/{owner}/{repo}/releases', {
     owner: 'meljameson',
     repo: 'test-gha',
@@ -13,20 +17,52 @@ async function run() {
     throw new Error(`Unable to get release data from GitHub. No response: ${JSON.stringify(response)}`)
   }
 
+  return response.data
+}
+
+/**
+ * 
+ * @returns {
+      url: string,
+      assets_url: string,
+      upload_url: string,
+      html_url: string,
+      id: number,
+      author: Object,
+      node_id: string,
+      tag_name: string,
+      target_commitish: string,
+      name: string,
+      draft: boolean,
+      prerelease: boolean,
+      created_at: string,
+      published_at: string,
+      assets: array,
+      tarball_url: string,
+      zipball_url: string,
+      body: string,
+    }
+ */
+async function getLastPreRelease() {
   // get the latest pre-release
-  const releases = response.data
+  const releases = await getAllReleases()
 
   if (releases == null) {
     throw new Error(`No releases found! releases: ${JSON.stringify(releases)}`)
   }
 
+  // the last release is the first in the release list
   const release = releases[0]
   
   if (release == null) {
     throw new Error(`Previous release not found! release: ${JSON.stringify(release)}`)
   }
 
-  const { id, name, prerelease } = release
+  return release
+}
+
+async function run() {
+  const { id, name, tag_name, prerelease } = await getLastPreRelease()
 
   if (id == null) {
     throw new Error(`Previous release id not found! release: ${JSON.stringify(release)}`)
@@ -36,7 +72,7 @@ async function run() {
     throw new Error(`Previous release was not a pre-release. Only pre-releases can be upgraded to a release. release: ${JSON.stringify(release)}`)
   }
 
-  console.log(`Updating release ${name} (id: ${id}) to latest`)
+  console.log(`Updating release ${name}:${tag_name} (id: ${id}) to latest`)
 
   await octokit.request('PATCH /repos/{owner}/{repo}/releases/{release_id}', {
     owner: 'meljameson',
@@ -46,6 +82,5 @@ async function run() {
     tag_name: 'release'
   })
 }
-
 
 run()
