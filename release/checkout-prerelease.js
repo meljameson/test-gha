937776aad0dcbaaee2e01334ef61c0115cde0619
Mel Jameson <mel.s.jameson@gmail.com>
2021-05-13 16:21:39 -0400

    remove err

--- release/checkout-prerelease.js
+++ release/checkout-prerelease.js
@@ -44,7 +44,6 @@
      return new Promise((resolve, reject) => {
          console.log('\x1b[36m%s\x1b[0m', `'${cmd}' is executing...`);
          exec(cmd, function (error, stdout, stderr) {
-           console.log({error, stdout, stderr})
              if (error) reject(error)
              if (stderr) reject(stderr)
              resolve(stdout)
